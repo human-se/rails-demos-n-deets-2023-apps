50941f06a12fe08e1ff7555bdd42257024f418ac
Marshall Lussier <marshallelussier@gmail.com>
2022-07-25 14:15:33 -0500

    Add Routes for Home and About Pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'home', to: 'pages#home', as: 'home'
+  get 'about', to: 'pages#about', as: 'about'
+  
 end
