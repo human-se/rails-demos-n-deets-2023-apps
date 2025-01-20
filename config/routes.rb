5d0e9579caf647c127b343098d205e96c83a35d6
Katie Bridson <kbridson@memphis.edu>
2025-01-20 16:25:57 -0600

    Add a Root Route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: redirect('/home')
+
   get 'home', to: 'pages#home', as: 'home'
   get 'about', to: 'pages#about', as: 'about'
 end
