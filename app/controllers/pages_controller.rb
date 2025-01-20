f61ffb4da34525da11991c5067e13b0875f33347
Katie Bridson <kbridson@memphis.edu>
2025-01-20 16:04:33 -0600

    Added about page to complete Billy Books base app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,8 @@ class PagesController < ApplicationController
   def home
     render :home
   end
+
+  def about
+    render :about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get 'home', to: 'pages#home', as: 'home'
+  get 'about', to: 'pages#about', as: 'about'
 end
