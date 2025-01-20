02d9d365e563e1019ef4e60998ca8e7214d5b0a0
Katie Bridson <kbridson@memphis.edu>
2025-01-20 17:09:52 -0600

    Add a Model Test for Each Validation

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: todos
@@ -9,10 +11,38 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
-require "test_helper"
+require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'fixtures must be valid' do
+    todos.each do |todo|
+      assert todo.valid?, todo.errors.full_messages.inspect
+    end
+  end
+
+  test 'title must be present' do
+    todo = todos(:one)
+    todo.title = ''
+    assert_not todo.valid?
+  end
+
+  test 'due date must be present' do
+    todo = todos(:one)
+    todo.due_date = nil
+    assert_not todo.valid?
+  end
+
+  test 'due date cannot be in the past' do
+    todo = todos(:one)
+    todo.due_date = Date.yesterday
+    assert_not todo.valid?
+  end
+  test 'due date can be in the future' do
+    todo = todos(:one)
+    todo.due_date = Date.tomorrow
+    assert todo.valid?
+  end
 end
