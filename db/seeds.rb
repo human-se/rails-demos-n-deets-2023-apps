b5d652fafcf4b10ed544555a9c2298265bb08ee8
Katie Bridson <kbridson@memphis.edu>
2025-01-20 16:32:21 -0600

    Seed the Database with Records

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Car.create!(
+  make:  'Dodge',
+  model: 'Viper',
+  color: 'Blue',
+  year:  1994,
+  price: 30_700.70
+)
+
+Car.create!(
+  make:  'Chevy',
+  model: 'Equinox',
+  color: 'Gold',
+  year:  2015,
+  price: 8327.99
+)
+
+Car.create!(
+  make:  'Tesla',
+  model: 'Model X',
+  color: 'Black',
+  year:  2020,
+  price: 77_641.00
+)
