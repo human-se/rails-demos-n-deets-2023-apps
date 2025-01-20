ad7cdda078d568a814a3365b97dbf2176e043c62
Katie Bridson <kbridson@memphis.edu>
2025-01-20 17:20:33 -0600

    Add an Update-Resource Route

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,5 @@ Rails.application.routes.draw do
   get 'todos/new', to: 'todos#new', as: 'new_todo'
   get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
   get 'todos/:id', to: 'todos#show', as: 'todo'
+  patch 'todos/:id', to: 'todos#update'
 end
