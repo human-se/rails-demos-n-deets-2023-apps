f8f47cc4c57eedcced19391152b94b6ec70473f5
Katie Bridson <kbridson@memphis.edu>
2025-01-20 17:17:32 -0600

    Add an Edit-Resource Route

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get 'todos', to: 'todos#index', as: 'todos'
   post 'todos', to: 'todos#create'
   get 'todos/new', to: 'todos#new', as: 'new_todo'
+  get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
   get 'todos/:id', to: 'todos#show', as: 'todo'
 end
