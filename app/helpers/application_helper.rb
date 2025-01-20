4d51bf814173925f421cda17ec13920fcda6450b
Katie Bridson <kbridson@memphis.edu>
2025-01-20 17:02:19 -0600

    Add Flash Notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  BOOTSTRAP_ALERT_CLASS = {
+    'success' => 'alert-success',
+    'error'   => 'alert-danger',
+    'notice'  => 'alert-info',
+    'alert'   => 'alert-danger',
+    'warn'    => 'alert-warning'
+  }.freeze
+
+  def alert_class(flash_key)
+    BOOTSTRAP_ALERT_CLASS.fetch(flash_key, 'alert-info')
+  end
 end
