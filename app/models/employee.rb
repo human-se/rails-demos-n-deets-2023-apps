25b41a1fc9a55f0471e423bc77d13163323bcf45
Katie Bridson <kbridson@memphis.edu>
2025-01-20 19:53:01 -0600

    Add has_many/belongs_to Declarations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -13,6 +13,16 @@
 #  updated_at :datetime         not null
 #
 class Employee < ApplicationRecord
+  has_many(
+    :project_assignments,
+    class_name:  'ProjectAssignment',
+    foreign_key: 'employee_id',
+    inverse_of:  :employee,
+    dependent:   :destroy
+  )
+
+  has_many :projects, through: :project_assignments
+
   validates :fname, :lname, :empid, presence: true
   validates :empid, format: { with: /\A\d{6}\z/ }
   validates :empid, uniqueness: true
