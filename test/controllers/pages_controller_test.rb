c60f9afb3f8258843895a5a42145de9c543ad9e5
Katie Bridson <kbridson@memphis.edu>
2025-01-20 20:42:18 -0600

    Add an Empty Controller Test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,12 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should get about' do
+  end
 end
