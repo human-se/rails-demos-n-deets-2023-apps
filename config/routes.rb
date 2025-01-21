3a136855bf5abbacd6f50e961ce64ffc6b22e9fa
Katie Bridson <kbridson@memphis.edu>
2025-01-20 19:34:50 -0600

    Create a Destroy Action for Deleting Existing Child Objects

--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,5 @@ Rails.application.routes.draw do
   get 'quizzes/:quiz_id/questions/:id', to: 'questions#show', as: 'quiz_question'
   get '/quizzes/:quiz_id/questions/:id/edit', to: 'questions#edit', as: 'edit_quiz_question'
   patch '/quizzes/:quiz_id/questions/:id', to: 'questions#update'
+  delete '/quizzes/:quiz_id/questions/:id', to: 'questions#destroy'
 end
